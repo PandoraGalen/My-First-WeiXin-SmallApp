bf726fdffba6757d0ab6b04e7341e29710bdf888
Pandora Galen <2350525451@qq.com>
2016-10-02 20:15:22 +0800

    v0.1
    简单小demo

---
+++ pages/cat/index.js
@@ -0,0 +1,195 @@
+Page({
+    data: {
+         array: [
+           {
+               mode: 'aspectFit',
+               src: "../../images/cat_4.png"
+           },
+           {
+               mode: 'aspectFit',
+               src: "../../images/search_2.png"
+           }
+       ],
+       lists: [{
+           mode: 'aspectFit',
+           src: '../../images/cat01.jpg',
+           jiage: '106.5 ',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat02.jpg',
+           jiage: '106.5 ',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat03.gif',
+           jiage: '106.5 ',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat04.jpg',
+           jiage: '106.5 ',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat05.jpg',
+           jiage: '106.5 ',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat06.gif',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat01.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat02.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat03.gif',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat04.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat05.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat06.gif',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat01.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat02.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat03.gif',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat04.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat05.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat06.gif',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat01.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat02.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat03.gif',
+           jiage: '106.5 ',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat04.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat05.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat06.gif',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat01.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat02.jpg',
+           jiage: '106.5',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat03.gif',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat04.jpg',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat05.jpg',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat06.gif',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat01.jpg',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat02.jpg',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat03.gif',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat04.jpg',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat05.jpg',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }, {
+            mode: 'aspectFit',
+           src: '../../images/cat06.gif',
+           jiage: '106.5 ￥',
+           details: '***********'
+       }]
+    }
+})
---
+++ pages/set/index.js
@@ -0,0 +1,56 @@
+Page({
+    data: {
+       array: [
+           {
+               mode: 'aspectFit',
+               src: "../../images/add_2.png"
+           },
+           {
+               mode: 'aspectFit',
+               src: "../../images/search_2.png"
+           }
+       ],
+       hd_logo: {
+           mode: 'sapectFit',
+           src: '../../images/mylogo.jpg'
+       },
+       hd_EWM: {
+            mode: 'sapectFit',
+           src: '../../images/head_EWM.png'
+       },
+       lists: [{
+           mode: 'aspectFit',
+           text: '向上 向上 向上',
+           src: '../../images/wenJian.png'
+       }, {
+           mode: 'aspectFit',
+           text: '向上 向上 向上',
+           src: '../../images/jiSuanQi.png'
+       }, {
+           mode: 'aspectFit',
+           text: '向上 向上 向上',
+           src: '../../images/riLi.png'
+       }, {
+           mode: 'aspectFit',
+           text: '向上 向上 向上',
+           src: '../../images/shuBao.png'
+       }, {
+           mode: 'aspectFit',
+           text: '向上 向上 向上',
+           src: '../../images/xinXi.png'
+       }, {
+           mode: 'aspectFit',
+           text: '向上 向上 向上',
+           src: '../../images/yiFu.png'
+       },{
+            mode: 'aspectFit',
+           text: '向上 向上 向上',
+           src: '../../images/weiZhi.png'
+       }, {
+            mode: 'aspectFit',
+           text: '向上 向上 向上',
+           src: '../../images/sheZhi.png'
+       }],
+       iconSrc: '../../images/right_icon.png'
+    }
+})
